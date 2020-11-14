041ec6a5f09afd9cf9588f71a0d35198b2044cb9
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-11-14 21:49:18 +0200

    Update ResultsActivity.java

--- app/src/main/java/com/example/contextmenu/ResultsActivity.java
+++ app/src/main/java/com/example/contextmenu/ResultsActivity.java
@@ -23,12 +23,12 @@ import android.widget.TextView;
  * @Since 1.1
  */
 
-public class ResultsActivity extends AppCompatActivity implements View.OnCreateContextMenuListener, AdapterView.OnItemClickListener
+public class ResultsActivity extends AppCompatActivity implements View.OnCreateContextMenuListener
 {
     ListView sequenceView;
     Intent gi;
     boolean type;
-    double a1,d, currentItem, previousItem;
+    double a1, d, currentItem, previousItem;
     double[] sums;
     String[] sequence;
     TextView label;
@@ -41,24 +41,23 @@ public class ResultsActivity extends AppCompatActivity implements View.OnCreateC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
-        sums=new double[20];
-        sequence=new String[20];
+        sums = new double[20];
+        sequence = new String[20];
 
-        sequenceView=(ListView)findViewById(R.id.sequenceView);
-        gi=getIntent();
-        type=gi.getBooleanExtra("type",false);
-        a1=gi.getDoubleExtra("a1",0);
-        d=gi.getDoubleExtra("d",0);
-        label=(TextView)findViewById(R.id.label);
-        resultview=(EditText)findViewById(R.id.resultview);
+        sequenceView = (ListView) findViewById(R.id.sequenceView);
+        gi = getIntent();
+        type = gi.getBooleanExtra("type", false);
+        a1 = gi.getDoubleExtra("a1", 0);
+        d = gi.getDoubleExtra("d", 0);
+        label = (TextView) findViewById(R.id.label);
+        resultview = (EditText) findViewById(R.id.resultview);
 
         sequenceView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        sequenceView.setOnItemClickListener(this);
 
         if (!type) arithmetic();
         else geometric();
 
-        ArrayAdapter<String> adp=new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, sequence);
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, sequence);
         sequenceView.setAdapter(adp);
         sequenceView.setOnCreateContextMenuListener(this);
     }
@@ -69,16 +68,16 @@ public class ResultsActivity extends AppCompatActivity implements View.OnCreateC
 
     public void arithmetic()
     {
-        currentItem=a1;
-        sums[0]=a1;
-        sequence[0]=currentItem+"";
+        currentItem = a1;
+        sums[0] = a1;
+        sequence[0] = currentItem + "";
 
-        for (int i=1; i<20; i++)
+        for (int i = 1; i < 20; i++)
         {
-            previousItem=currentItem;
-            currentItem=previousItem+d;
-            sums[i]=sums[i-1]+currentItem;
-            sequence[i]=currentItem+"";
+            previousItem = currentItem;
+            currentItem = previousItem + d;
+            sums[i] = sums[i - 1] + currentItem;
+            sequence[i] = currentItem + "";
         }
     }
 
@@ -88,21 +87,20 @@ public class ResultsActivity extends AppCompatActivity implements View.OnCreateC
 
     public void geometric()
     {
-        currentItem=a1;
-        sums[0]=a1;
-        sequence[0]=currentItem+"";
+        currentItem = a1;
+        sums[0] = a1;
+        sequence[0] = currentItem + "";
 
-        for (int i=1; i<20; i++)
+        for (int i = 1; i < 20; i++)
         {
-            previousItem=currentItem;
-            currentItem=previousItem*d;
-            sums[i]=sums[i-1]+currentItem;
-            sequence[i]=currentItem+"";
+            previousItem = currentItem;
+            currentItem = previousItem * d;
+            sums[i] = sums[i - 1] + currentItem;
+            sequence[i] = currentItem + "";
         }
     }
 
     /**
-     *
      * @param menu
      * @param v
      * @param menuInfo
@@ -118,7 +116,6 @@ public class ResultsActivity extends AppCompatActivity implements View.OnCreateC
     }
 
     /**
-     *
      * @param item
      * @return shows the results on the screen
      */
@@ -126,40 +123,24 @@ public class ResultsActivity extends AppCompatActivity implements View.OnCreateC
     @Override
     public boolean onContextItemSelected(MenuItem item)
     {
-        String s=item.getTitle().toString();
+        String s = item.getTitle().toString();
+
+        AdapterView.AdapterContextMenuInfo menuInfo=(AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        position=menuInfo.position+1;
 
         if (s.equals("It's position"))
         {
             label.setText("n=");
-            resultview.setText(""+position);
+            resultview.setText("" + position);
             return true;
         }
         if (s.equals("The total of all items till this one"))
         {
             label.setText("Sn=");
-            resultview.setText(""+sums[position-1]);
+            resultview.setText("" + sums[position - 1]);
         }
 
         return super.onContextItemSelected(item);
-    }
-
-    /**
-     * Callback method to be invoked when an item in this AdapterView has
-     * been clicked.
-     * <p>
-     * Implementers can call getItemAtPosition(position) if they need
-     * to access the data associated with the selected item.
-     *
-     * @param parent   The AdapterView where the click happened.
-     * @param view     The view within the AdapterView that was clicked (this
-     *                 will be a view provided by the adapter)
-     * @param position The position of the view in the adapter.
-     * @param id       The row id of the item that was clicked.
-     */
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-    {
-        this.position=position+1;
     }
 
     public void back(View view)
